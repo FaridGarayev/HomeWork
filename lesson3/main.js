7d88d3295420f6c9b1591c9af22c303bdc10f03e
Farid Garayev <61631637+b1ackBug@users.noreply.github.com>
2021-05-04 11:11:13 +0400

    Add files via upload

---
+++ lesson3/main.js
@@ -0,0 +1,70 @@
+$(function(){
+    $("#p1").click(function(){
+      $("#p2").hide(2000);
+    });
+    $("#p1").click(function(){
+        $("#p2").show(2000);
+      });
+  });
+
+document.querySelectorAll(".page3-box")[1].style.backgroundColor = "#E71B37";
+document.querySelectorAll(".page3-box")[1].style.color = "white";
+document.querySelectorAll(".downIconJs")[1].style.display = "block";
+
+var card = [
+  ["Kloe Len","https://images-ext-2.discordapp.net/external/ZvWPWIznkTj4ho6JGsnMzA_vzwqrv4AvAWr0WYC6n6U/https/preview.templatehouse.net/eventick/demo/images/team-01.jpg"],
+  ["Olivia Butler","https://images-ext-2.discordapp.net/external/MjM2ywZKS0n0YBHWfnEyARPxovRh5wvzF9qtnLRqlvY/https/preview.templatehouse.net/eventick/demo/images/team-02.jpg"],
+  ["Terasa Flores","https://images-ext-1.discordapp.net/external/-FHrOHWRQ3_km1e7TRbHBDgNHtbZHltYjmLVd0gRvqg/https/preview.templatehouse.net/eventick/demo/images/team-03.jpg"],
+  ["Gemma Bell","https://images-ext-1.discordapp.net/external/cELaGdm_ixodDuHn3yJ6ZwAFScArRIIE-Vc93xvdvn8/https/preview.templatehouse.net/eventick/demo/images/team-04.jpg"]
+]
+
+
+for (let i = 0; i<4 ; i++) {
+  document.querySelectorAll(".page3-box")[i].addEventListener("click", boxJsIn);
+  function boxJsIn(){
+    document.querySelectorAll(".page3-box")[i].style.backgroundColor = "#E71B37";
+    document.querySelectorAll(".page3-box")[i].style.color = "white";
+    document.querySelectorAll(".downIconJs")[i].style.display = "block";
+    document.querySelector(".page3-cards").style.display = "block";
+    document.getElementById("page3-name").innerHTML = card[i][0];
+    document.getElementById("page3-photo").src = card[i][1];
+
+      for(let j=0;j<4;j++)
+      {
+          if(j!=i){
+            document.querySelectorAll(".page3-box")[j].style.backgroundColor = "transparent";
+            document.querySelectorAll(".page3-box")[j].style.color = "black";
+            document.querySelectorAll(".downIconJs")[j].style.display = "none";
+          }
+      }
+  }
+}
+
+
+
+$(document).ready(function() {
+  $("#flip").click(function() {
+      $("#panel").slideToggle("slow");
+  });
+});
+
+$(document).ready(function() {
+  $("#flip1").click(function() {
+      $("#panel1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function() {
+  $("#flip2").click(function() {
+      $("#panel2").slideToggle("slow");
+  });
+});
+
+
+
+
+$(document).ready(function() {
+  $("#speakersID").hover(function() {
+      $(".speakersJsin").slideToggle("slow");
+  });
+});
